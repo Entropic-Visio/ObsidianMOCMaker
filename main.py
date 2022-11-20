92535589b0016e9d346e4de582d92aaf77f8dba2
Entropic-Visio <108833542+Entropic-Visio@users.noreply.github.com>
2022-11-20 02:03:37 +0000

    Add files via upload

--- main.py
+++ main.py
@@ -1,1 +1,24 @@
+import os
+import markdown
+from pathlib import Path
 
+
+# Obsidian Vault
+basepath = Path('C:\\Users\\entro\\OneDrive\\Documents\\Kaiser')
+
+# MOC Path
+indexPath = 'C:\\Users\\entro\\OneDrive\\Documents\\Kaiser\\SETTINGS\\_INDEX\\INDEX.md'
+    
+pathlist = Path(basepath).rglob('*.md')
+
+# REMOVES ALL CONTENT IN INDEX.md
+with open(indexPath, "r+") as file:
+    file.truncate(0)
+
+# APPENDS ALL MD Files into INDEX
+for path in pathlist:
+     # because path is object not string
+     path_in_str = str(path)
+     baseFilename = os.path.splitext(os.path.basename(path))[0]
+     with open(indexPath, 'a+') as f:
+        f.write(f"[[{baseFilename}]]\n")
